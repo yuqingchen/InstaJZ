22988576edd606a59016c5329b68f0e453cbb1b4
Yuqing Chen <chenyuqing0320@gmail.com>
2020-07-16 00:35:07 -0700

    version one login logout like profile following_filter

--- Insta/urls.py
+++ Insta/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from Insta.views import HelloWrold, PostsView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from Insta.views import HelloWrold, PostsView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, addLike, UserDetailView
 
 urlpatterns = [
-    path('', HelloWrold.as_view(), name='home'),
-    path('posts/', PostsView.as_view(), name="posts"),
+    path('home', HelloWrold.as_view(), name='home'),
+    path('', PostsView.as_view(), name="posts"),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('posts/new/', PostCreateView.as_view(), name='make_post'),
     path('posts/update/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
     path('posts/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
+    path('like', addLike, name='addLike'),
+    path('user/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
 ]
